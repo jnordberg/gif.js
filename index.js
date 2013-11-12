2a20675ff6c1daba4aa3641839db3147c38c29d5
Johan Nordberg <its@johan-nordberg.com>
2013-11-12 14:26:52 +1100

    0.1.5 (also on npm)

---
+++ index.js
@@ -0,0 +1,14 @@
+/*     _    ___  _
+      (_)  / __)(_)
+  ____ _ _| |__  _  ___
+ / _  | (_   __)| |/___)
+( (_| | | | | _ | |___ |
+ \___ |_| |_|(_)| (___/
+(_____|       (_*/
+
+module.exports = {
+  NeuQuant: require('./src/NeuQuant.js'),
+  TypedNeuQuant: require('./src/TypedNeuQuant.js'),
+  GIFEncoder: require('./src/GIFEncoder.js'),
+  LZWEncoder: require('./src/LZWEncoder.js')
+};
